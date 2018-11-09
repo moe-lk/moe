a6b992bdcc223bf0078b3946fa129713ce38a8fe
nizar <nizarucsc@gmail.com>
2018-11-09 10:43:40 +0530

    fix placement save issue

---
+++ employee-service/php-fpm/db/migrations/20181109050701_add_service_sector_to_service_table.php
@@ -0,0 +1,45 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class AddServiceSectorToServiceTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up()
+    {
+        $table = $this->table('Service');
+        $table->addColumn('service_sector', 'string',['limit'=>15]);
+        $table->update();
+    }
+    public function down()
+    {
+        $table = $this->table('Service');
+        $table->removeColumn('service_sector');
+        $table->update();
+    }
+}
